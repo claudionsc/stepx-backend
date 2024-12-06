a9a47de33ba5b5133205189a8bbbdc6e7a84f731
claudionsc <claudio,alves20112011@hotmail.com>
2024-12-06 06:35:47 -0300

    feat(src\shoes\DTO\shoes.d.ts): Criando DTO para os tenis

---
+++ src/shoes/DTO/shoes.d.ts
@@ -0,0 +1,14 @@
+export type Shoe = {
+    key: string;
+    id: number;
+    nome: string;
+    preco: number;
+    img: {
+      img01: string;
+      img02: string;
+      img03: string;
+      img04: string;
+    };
+    tamanhos: number[];
+  };
+  
